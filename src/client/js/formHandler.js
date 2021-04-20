05220480fbedc3290685aa498082e95f56369138
GoodnessObi <goodnessobi@gmail.com>
2021-04-20 12:09:47 +0100

    feature: geonames api consumed

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,32 +3,64 @@ const apiKey = 'ebbd2f57d5cadca45a8ea3aa17c0b066';
 
 /* Function called by event listener */
 function performAction (){
-  const zipCode = document.getElementById('zip').value;
-  const feeling = document.getElementById('feelings').value;
+  // const city = document.getElementById('city').value;
+  const city = 'Lagos';
+  const feeling = document.getElementById('country').value;
+
+  console.log("::: Form Submitted :::", city) 
+  fetch(`http://localhost:8000/addData`, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({ city }),
+  })
+  .then(res => res.json())
+  .then(function(res) {
+      console.log(res)
+      if (res.error) {
+          throw new Error(res.error);
+      }
+      
+      Client.clearUI();
+      // document.getElementById('name').value = '';
+      // document.querySelector('.results').classList.add("fadeIn");
+      // document.getElementById('url').innerHTML = formText;
+      // document.getElementById('confidence').innerHTML = res.confidence.toLowerCase();
+      // document.getElementById('agreement').innerHTML = res.agreement.toLowerCase();
+      // document.getElementById('irony').innerHTML = res.irony.toLowerCase();
+      // document.getElementById('subjective').innerHTML = res.subjectivity.toLowerCase();
+  })
+  .catch(error => {
+      console.log(error)
+      // Client.clearUI();
+      // document.querySelector('#error').classList.add("fadeIn");
+  })
   
-  Client.clearRecentEntry();
-  getWeather(apiKey, zipCode)
-    .then(function(data) {
-      return postData('/', {...data, feeling})
-    })
-    .then(function(){
-      return Client.updateUI();
-    }).then(function() {
-      return Client.clearInputFields();
-    })
-    .catch(function(error) {
-      document.getElementById('error').innerHTML = error.message;
-    })
+  // Client.clearRecentEntry();
+  // getWeather(apiKey, zipCode)
+  //   .then(function(data) {
+  //     return postData('/', {...data, feeling})
+  //   })
+  //   .then(function(){
+  //     return Client.updateUI();
+  //   }).then(function() {
+  //     return Client.clearInputFields();
+  //   })
+  //   .catch(function(error) {
+  //     document.getElementById('error').innerHTML = error.message;
+    // })
 }
 
 /* Function to GET Web API Data*/
-async function getWeather (apiKey, zipCode){
+async function getWeather (){
   try {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${apiKey}&units=imperial`)
+    const response = await fetch('http://api.geonames.org/searchJSON?q=Lagos&country=PT&username=goodness')
     const data = await response.json();
-    return {temp: data.main.temp, feels: data.main.feels_like, country: data.name};
+    console.log('successful call made')
+    console.log('country:' + data.geonames[0].countryName + ', latitude:' + data.geonames[0].lat + ', longitude:' + data.geonames[0].lng)
+    return {latitude: data.geonames[0].lat, longitude: data.geonames[0].long}
+    // return {temp: data.main.temp, feels: data.main.feels_like, country: data.name};
   } catch(error) {
-    throw new Error('Data unavailabe for that zipcode');
+    // throw new Error('Data unavailabe for that zipcode');
   }
 }
 
